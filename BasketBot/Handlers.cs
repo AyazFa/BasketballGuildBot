ee3bc41dfb20bea0fd4ad6ef08d9204cbf3303e1
Файзуллин Аяз Алмасович <Fayzullin.AA@gazprom-neft.ru>
2022-03-21 17:25:37 +0500

    refactor: handlers and callback methods for commands

---
+++ BasketBot/Handlers.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace BasketBot
+{
+    public class Handlers
+    {
+        public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            var handler = update.Type switch
+            {
+                // UpdateType.Unknown:
+                // UpdateType.ChannelPost:
+                // UpdateType.EditedChannelPost:
+                // UpdateType.ShippingQuery:
+                // UpdateType.PreCheckoutQuery:
+                // UpdateType.Poll:
+                UpdateType.Message            => BotOnMessageReceived(botClient, update.Message!, cancellationToken),
+                // UpdateType.EditedMessage      => BotOnMessageReceived(botClient, update.EditedMessage!),
+                // UpdateType.CallbackQuery      => BotOnCallbackQueryReceived(botClient, update.CallbackQuery!),
+                // UpdateType.InlineQuery        => BotOnInlineQueryReceived(botClient, update.InlineQuery!),
+                // UpdateType.ChosenInlineResult => BotOnChosenInlineResultReceived(botClient, update.ChosenInlineResult!),
+                // _                             => UnknownUpdateHandlerAsync(botClient, update)
+            };
+
+            try
+            {
+                await handler;
+            }
+            catch (Exception exception)
+            {
+                await HandleErrorAsync(botClient, exception, cancellationToken);
+            }
+        }
+
+        public static Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            var ErrorMessage = exception switch
+            {
+                ApiRequestException apiRequestException
+                    => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                _ => exception.ToString()
+            };
+
+            Console.WriteLine(ErrorMessage);
+            return Task.CompletedTask;
+        }
+
+        private static async Task BotOnMessageReceived(ITelegramBotClient botClient, Message message, CancellationToken cancellationToken)
+        {
+            // Only process text messages
+            if (message.Type != MessageType.Text)
+                return;
+
+            var chatId = message.Chat.Id;
+            var messageText = message.Text;
+
+            Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
+
+            var sentMessage = messageText switch
+            {
+                "/sendpoll" => await botClient.SendPollAsync(
+                    chatId: chatId,
+                    question: "Играем завтра на АСФ?",
+                    options: new []
+                    {
+                        "Да",
+                        "Нет"
+                    },
+                    cancellationToken: cancellationToken),
+                _ => await botClient.SendTextMessageAsync(
+                    chatId: chatId,
+                    text: "You said:\n" + messageText,
+                    cancellationToken: cancellationToken),
+            };
+        }
+    }
+}
--- BasketBot/Program.cs
+++ BasketBot/Program.cs
@@ -2,10 +2,7 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Telegram.Bot;
-using Telegram.Bot.Exceptions;
 using Telegram.Bot.Extensions.Polling;
-using Telegram.Bot.Types;
-using Telegram.Bot.Types.Enums;
 
 namespace BasketBot
 {
@@ -24,8 +21,8 @@ namespace BasketBot
             };
             botClient.StartReceiving
             (
-                HandleUpdateAsync,
-                HandleErrorAsync,
+                Handlers.HandleUpdateAsync,
+                Handlers.HandleErrorAsync,
                 receiverOptions,
                 cancellationToken: cts.Token);
 
@@ -36,40 +33,6 @@ namespace BasketBot
 
             // Send cancellation request to stop bot
             cts.Cancel();
-
-            async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
-            {
-                // Only process Message updates: https://core.telegram.org/bots/api#message
-                if (update.Type != UpdateType.Message)
-                    return;
-                // Only process text messages
-                if (update.Message!.Type != MessageType.Text)
-                    return;
-
-                var chatId = update.Message.Chat.Id;
-                var messageText = update.Message.Text;
-
-                Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
-
-                // Echo received message text
-                Message sentMessage = await botClient.SendTextMessageAsync(
-                    chatId: chatId,
-                    text: "You said:\n" + messageText,
-                    cancellationToken: cancellationToken);
-            }
-
-            Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
-            {
-                var ErrorMessage = exception switch
-                {
-                    ApiRequestException apiRequestException
-                        => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
-                    _ => exception.ToString()
-                };
-
-                Console.WriteLine(ErrorMessage);
-                return Task.CompletedTask;
-            }
         }
     }
 }
