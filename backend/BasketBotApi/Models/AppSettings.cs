d570dbf10f030d580c6723cbe8011be3c3c1019c
DESKTOP-T6O7O0P\gar21 <gar21@yandex.ru>
2024-08-05 23:06:15 +0500

    Added member info and new chat

--- backend/BasketBotApi/Models/AppSettings.cs
+++ backend/BasketBotApi/Models/AppSettings.cs
@@ -7,9 +7,10 @@ public class AppSettings
     public static string Key { get; set; } = "5109422101:AAEG6S0XNhimPs6C3MPPSHifM4LBV8fYMYM";
 
     /// <summary>
-    /// -1001730302996 - баскетбольная группа
+    /// -1001730302996 - баскетбольная группа DreamTeam Ufa
+    /// -1002211978434 - баскетбольная группа ГПН
     /// -654532921 - тестовая группа с ботом
     /// -956790495 - идентификатор бота
     /// </summary>
-    public static long[] GuildChatIds { get; set; } = new[] { -1001730302996, -654532921, -654532922};
+    public static long[] GuildChatIds { get; set; } = new[] { -1001730302996, -1002211978434, -654532921, -654532922};
 }
