9a31e5b9d42d992249d63b91c456d5d10685e0b9
Ayaz Fayzullin <gar21@yandex.ru>
2025-04-18 14:35:40 +0500

    usage message on start command and stub for stop poll command

--- backend/BasketBotApi/Models/Commands/StartCommand.cs
+++ backend/BasketBotApi/Models/Commands/StartCommand.cs
@@ -1,6 +1,7 @@
 ﻿using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
 namespace BasketBotApi.Models.Commands;
 
@@ -18,7 +19,17 @@ public class StartCommand : Command
 
     public override async Task Execute(Message message, TelegramBotClient botClient, long chatId)
     {
+        const string usageText = """
+                                 <b><u>Commands</u></b>:
+                                 /cp    - CreatePoll. Создать опрос. /cp1 - опрос для АСФ; /cp2 - опрос для Твой зал
+                                 /tj    - TakeJersey. Отправка напоминания необходимости взять темную и светлую формы
+                                 /cm    - CollectMoney. Отправка сообщения со сбором денег, /cmamount, где amount - сумма для сбора, например /cm300
+                                 /sp    - StopPoll. Остановить опрос.
+                                 /mt    - MakeTeams. Сформировать команды из проголосовавших в опросе.
+                             """;
         var messageChatId = message.Chat.Id;
-        await botClient.SendTextMessageAsync(messageChatId, "Basketball is the best game with ball", parseMode: Telegram.Bot.Types.Enums.ParseMode.Markdown);
+        await botClient.SendTextMessageAsync(messageChatId,
+            usageText,
+            parseMode: ParseMode.Html);
     }
 }
---
+++ backend/BasketBotApi/Models/Commands/StopPollCommand.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace BasketBotApi.Models.Commands;
+
+/// <summary>
+/// Команда завершения опроса для посещения тренировки
+/// /sp - Stop Poll
+/// </summary>
+public class StopPollCommand : Command
+{
+    public override string Name => @"/sp";
+    public override Task Execute(Message message, TelegramBotClient botClient, long chatId)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override bool Contains(Message message)
+    {
+        throw new System.NotImplementedException();
+    }
+}
